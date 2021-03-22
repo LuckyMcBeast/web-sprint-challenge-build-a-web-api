7143e2c692a11a15c40e97a7f17e873133c26e04
M. Cullen McClellan <cullenmcclellan@gmail.com>
2021-03-21 23:15:35 -0400

    Actions complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -50,27 +50,55 @@ router.post('/', async (req, res) => {
         })
     }
     else {
-        try {
-            await Actions.insert(newAction)
-                .then(action => {
-                    res.status(201).json(action)
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({ message: "Server Error" })
-                })
-        }
-        catch (err) {
-            res.status(400).json(err)
-        }
+        Actions.insert(newAction)
+            .then(action => {
+                res.status(201).json(action)
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ message: "Server Error" })
+            })
     }
+
 })
 
 router.put('/:id', (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const changes = req.body;
 
-    if(!)
+    if (!changes.project_id || !changes.description || !changes.notes) {
+        res.status(400).json({
+            message: "New actions must include project_id, description, and notes"
+        })
+    }
+    else{ 
+        Actions.update(id, changes)
+        .then(action => {
+            res.status(201).json(action)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ message: "Server Error" });
+        })
+}})
+
+router.delete('/:id' , async (req, res) => {
+    const { id } = req.params;
+    try {
+        const action = await Actions.remove(id);
+        if (action) {
+            res.status(200).json({ message: `${id} was deleted.` })
+        }
+        else {
+            res.status(404).json({ message: "Action Not Found" })
+        }
+    }
+    catch (err) {
+        res.status(500).json({ message: `Action could not be removed.`, error: err.message })
+    }
 })
+
+
+
 
 module.exports = router;
